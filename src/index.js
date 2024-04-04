3d93cd10dba222ac6c56c29dbb1a7f0f3148e400
Alex Everman <alexeverman@Alexs-MacBook-Air.local>
2024-04-04 19:31:37 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//bootstrap import
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//code wasn't working with React.StrictMode so deleted
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(React.createElement(App, {}, null));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
